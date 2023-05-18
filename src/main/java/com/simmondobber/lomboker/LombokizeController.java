449634793541fbad995bc275d9f66de8ffaaebdc
simmondobber <wojciechfbaranowski@gmail.com>
2023-05-18 03:18:24 +0200

    Lombokize controller and service

---
+++ src/main/java/com/simmondobber/lomboker/LombokizeController.java
@@ -0,0 +1,25 @@
+package com.simmondobber.lomboker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LombokizeController {
+
+    private final LombokizeService lombokizeService;
+
+    @Autowired
+    public LombokizeController(LombokizeService lombokizeService) {
+        this.lombokizeService = lombokizeService;
+    }
+
+    @PostMapping("/lombokize")
+    public ResponseEntity<String> lombokize(@RequestBody String classCode) {
+        return new ResponseEntity<>(this.lombokizeService.lombokize(classCode), HttpStatus.OK);
+    }
+
+}
