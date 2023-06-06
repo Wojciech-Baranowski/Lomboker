294c07bf3fb974f5eee96708b82f219db831610a
wojbara1 <wojbara1@pg.edu.pl>
2023-06-06 15:05:52 +0200

    Amendments after first QA

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -13,7 +13,7 @@ import {CookieService} from "ngx-cookie-service";
 
 export class AppComponent implements OnInit {
 
-    private LOMBOKIZE_URL: string = 'http://localhost:8080/lombokize';
+    private LOMBOKIZE_URL: string = 'http://localhost:3821/lombokize';
     private backgroundName: string;
 
     headerBackground: string;
