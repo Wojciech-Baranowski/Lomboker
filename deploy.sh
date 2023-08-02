cb6cff26cf49d4018ec65cc0375272bfa5dcaa18
simmondobber <wojciechfbaranowski@gmail.com>
2023-08-03 00:25:20 +0200

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-Backend/mvnw package
 docker build Backend/ -t lomboker:lombokerbackend
 docker build Frontend/ -t lomboker:lombokerfrontend
+docker tag lomboker:lombokerbackend simmondobber/lombokerbackend
+docker tag lomboker:lombokerfrontend simmondobber/lombokerfrontend
 docker push simmondobber/lombokerbackend
 docker push simmondobber/lombokerfrontend
 docker-compose pull
