e2788d9ae0661ad661475bf2b8d7f827a7347e1f
wojbara1 <wojbara1@pg.edu.pl>
2023-08-16 14:48:48 +0200

    nginx

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-docker build Backend/ -t lomboker:lombokerbackend
-docker build Frontend/ -t lomboker:lombokerfrontend
-docker tag lomboker:lombokerbackend simmondobber/lombokerbackend
-docker tag lomboker:lombokerfrontend simmondobber/lombokerfrontend
-docker push simmondobber/lombokerbackend
-docker push simmondobber/lombokerfrontend
-docker-compose pull
-docker-compose up
